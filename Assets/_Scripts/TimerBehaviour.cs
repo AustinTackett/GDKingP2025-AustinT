e0d9f38e21cbc72d64860bfba3d7d3cd18a69b89
AustinTackett <117427861+AustinTackett@users.noreply.github.com>
2025-01-29 15:45:31 -0600

    Added Timer

---
+++ Assets/_Scripts/TimerBehaviour.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+
+public class TimerBehaviour : MonoBehaviour
+{
+    private float timer;
+    private TextMeshProUGUI textfield;
+
+    void Start()
+    {
+        textfield = GetComponent<TextMeshProUGUI>();
+
+        if (textfield == null) 
+        {
+            Debug.Log("No text component found!");
+        }
+    }
+
+    void Update()
+    {
+        timer = Time.time;
+        
+
+        if (textfield != null)
+        {
+            int minutes = Mathf.FloorToInt(timer/ 60);
+            int seconds = Mathf.FloorToInt(timer % 60);
+            string timeLabel = string.Format("<color=black>Time: <color=#0080ff>{0:00}<color=black>:<color=#0080ff>{1:00}", minutes, seconds);
+            textfield.text = timeLabel;
+        }
+        
+        //Debug.Log(timer);
+    }
+}
